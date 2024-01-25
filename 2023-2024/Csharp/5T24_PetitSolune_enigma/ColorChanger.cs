d482fd9d15867ba9a2ecf92d7352695f3274f277
Solune-Petit <210225@site.asty-moulin.be>
2024-01-25 19:20:42 +0100

    .

---
+++ 2023-2024/Csharp/5T24_PetitSolune_enigma/ColorChanger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5T24_PetitSolune_enigma
+{
+    internal class ColorChanger
+    {
+        public static void red()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+
+        public static void blue()
+        {
+            Console.ForegroundColor= ConsoleColor.Blue;
+        }
+
+        public static void green()
+        {
+            Console.ForegroundColor= ConsoleColor.Green;
+        }
+
+        public static void cyan()
+        {
+            Console.ForegroundColor=ConsoleColor.Cyan;
+        }
+
+        public static void white()
+        {
+            Console.ForegroundColor= ConsoleColor.White;
+        }
+
+        public static void black()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        public static void yellow()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.;
+        }
+
+        public static void darkblue()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+        }
+
+        public static void darkcyan()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+        }
+
+    }
+}
---
+++ 2023-2024/Csharp/5T24_PetitSolune_enigma/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _5T24_PetitSolune_enigma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+}
